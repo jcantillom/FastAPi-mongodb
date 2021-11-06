bc9a967c632bab22520b33ea7f910cca3c4b7e90
JUAN CANTILLO <juanjo0216@gmail.com>
2021-11-06 01:14:40 -0500

    tags
    Add method in tags

--- routes/user.py
+++ routes/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, status
 from pymongo.auth import _password_digest
 from config.db import conex
 from schemas.user import userEntity, usersEntity
@@ -13,31 +13,30 @@ from fastapi import FastAPI, HTTPException
 user = APIRouter()
 
 # METODO GetUser
-@user.get('/users', response_model=list[User])
+@user.get('/users', response_model=list[User], tags=['USERS'])
 def find_all_user():
     return usersEntity(conex.local.user.find())
 
 
 # METODO CreateUser
-@user.post('/users', response_model=User)
+@user.post('/users', response_model=User, tags=['USERS'])
 def create_user(user: User):
     new_user = dict(user)
     new_user['password'] = sha256_crypt.encrypt(new_user['password'])
     del new_user['id']
-
     id = conex.local.user.insert_one(new_user).inserted_id
     user = conex.local.user.find_one({'_id': id})
     return userEntity(user)
 
 
 # METODO GetUser
-@user.get('/users/{id}')
+@user.get('/users/ {id}', response_model=list[User], tags=['USERS'])
 def find_user(id: str):
     return userEntity(conex.local.user.find_one({"_id": ObjectId(id)}))
 
 
 # METODO Login
-@user.post('/login')
+@user.post('/login', tags=['LOGIN'])
 def user_login(auth: Auth):
   try:
     user = userEntity(conex.local.user.find_one({"mobil_phone": auth.mobil_phone}))
@@ -51,7 +50,7 @@ def user_login(auth: Auth):
 
 
 # METODO UPDATE (PUT)
-@user.put('/users/{id}')
+@user.put('/users/{id}', response_model=User, tags=['USERS'])
 def update_user(id: str, user: User):
     conex.local.user.find_one_and_update(
         {'_id': ObjectId(id)}, {'$set': dict(user)})
@@ -59,7 +58,7 @@ def update_user(id: str, user: User):
 
 
 # METODO DELETE
-@user.delete('/users/{id}')
+@user.delete('/users/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['USERS'])
 def delete_user():
     userEntity(conex.local.user.find_one({"_id": ObjectId(id)}))
     return Response(status_code=HTTP_204_NO_CONTENT)
